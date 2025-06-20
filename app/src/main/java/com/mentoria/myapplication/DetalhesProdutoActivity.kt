b0b8b65bd91453eabf6ed3971bda875f97413cfe
BRADDTELES <braddteles@gmail.com>
2025-06-20 09:22:57 -0300

    feat: criar layout da tela de detalhes do produto

---
+++ app/src/main/java/com/mentoria/myapplication/DetalhesProdutoActivity.kt
@@ -0,0 +1,11 @@
+package com.mentoria.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetalhesProdutoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes_produto)
+    }
+}
